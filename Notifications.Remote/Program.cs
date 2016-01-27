b29fc7d2653a6aae5faad0afd2e81b37769ad1ad
Akın Tarakcı <akin@armut.com>
2016-01-27 17:35:58 +0200

    remote project and nuget updates

---
+++ Notifications.Remote/Program.cs
@@ -0,0 +1,13 @@
+﻿using Akka.Actor;
+
+namespace Notifications.Remote
+{
+    class Program
+    {
+        private static ActorSystem NotificationsActorSystem;
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
